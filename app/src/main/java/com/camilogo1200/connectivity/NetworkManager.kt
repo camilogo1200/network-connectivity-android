71806b25cfd4a34a71f678427891f37fc11efef1
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2022-07-22 01:30:40 -0500

    WIP: Meaningful tags

--- app/src/main/java/com/camilogo1200/connectivity/NetworkManager.kt
+++ app/src/main/java/com/camilogo1200/connectivity/NetworkManager.kt
@@ -8,11 +8,15 @@ import androidx.annotation.RequiresApi
 
 object NetworkManager {
 
+
+
+
     private val TAG = NetworkManager::class.java.name
     fun isNetworkAvailable(context: Context): Boolean {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) isConnectivityAvailable(context)
         else isConnectivityAvailableLegacy(context)
     }
+
 
     private fun isConnectivityAvailableLegacy(context: Context): Boolean {
         val connMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -41,28 +45,36 @@ object NetworkManager {
         connectivityManager.registerDefaultNetworkCallback(
             object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
-                    Log.e(TAG, "The default network is now: " + network)
+                    //Log.e(TAG, "The default network is now: $network")
+                    Log.e(TAG, "onAvailable()")
                 }
 
                 override fun onLost(network: Network) {
-                    Log.e(
-                        TAG,
-                        "The application no longer has a default network. The last default network was " + network
-                    )
+
+                    Log.e(TAG, "onLost()")
+//                    Log.e(
+//                        TAG,
+//                        "The application no longer has a default network. The last default network was $network"
+                    //)
                 }
 
                 override fun onCapabilitiesChanged(
                     network: Network,
                     networkCapabilities: NetworkCapabilities
                 ) {
-                    Log.e(TAG, "The default network changed capabilities: " + networkCapabilities)
+                    val internet =
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    val validated =
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                    Log.e(TAG, "Internet: $internet - Validated: $validated")
+                    //Log.e(TAG, "The default network changed capabilities: $networkCapabilities")
                 }
 
                 override fun onLinkPropertiesChanged(
                     network: Network,
                     linkProperties: LinkProperties
                 ) {
-                    Log.e(TAG, "The default network changed link properties: " + linkProperties)
+                    //Log.e(TAG, "The default network changed link properties: $linkProperties")
                 }
             })
     }
