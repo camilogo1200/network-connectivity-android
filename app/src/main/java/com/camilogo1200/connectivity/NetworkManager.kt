121ec378d660d679da45559df17282f1c045bb96
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2022-07-23 21:44:17 -0500

    WIP: Listeners

--- app/src/main/java/com/camilogo1200/connectivity/NetworkManager.kt
+++ app/src/main/java/com/camilogo1200/connectivity/NetworkManager.kt
@@ -6,17 +6,29 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 
-object NetworkManager {
+class NetworkManager(private val context: Context) {
+
+    private val tag = NetworkManager::class.java.name
+    private lateinit var onLostListener: () -> Unit
+    private lateinit var onAvailableListener: () -> Unit
+
+    init {
+        setUpNetworkListeners()
+    }
+
+    fun setOnAvailableNetworkListener(onAvailableListener: () -> Unit) {
+        this.onAvailableListener = onAvailableListener
+    }
+
+    fun setOnLostNetworkListener(onLostListener: () -> Unit) {
+        this.onLostListener = onLostListener
+    }
 
 
-
-
-    private val TAG = NetworkManager::class.java.name
-    fun isNetworkAvailable(context: Context): Boolean {
+    fun isNetworkAvailable(): Boolean { //inject context
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) isConnectivityAvailable(context)
         else isConnectivityAvailableLegacy(context)
     }
-
 
     private fun isConnectivityAvailableLegacy(context: Context): Boolean {
         val connMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -26,13 +38,10 @@ object NetworkManager {
 
     @RequiresApi(Build.VERSION_CODES.S)
     private fun isConnectivityAvailable(context: Context): Boolean {
+
         val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
-
         val network = connectivityManager.activeNetwork
-
-        val capabilities = connectivityManager.getNetworkCapabilities(network)
-
-        capabilities?.let {
+        connectivityManager.getNetworkCapabilities(network)?.let {
             val internetConnection = it.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             val validatedNetwork = it.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
             return (internetConnection && validatedNetwork)
@@ -40,18 +49,23 @@ object NetworkManager {
         return false
     }
 
-    fun networkListener(context: Context) {
+    private fun setUpNetworkListeners() {
         val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
         connectivityManager.registerDefaultNetworkCallback(
             object : ConnectivityManager.NetworkCallback() {
+
                 override fun onAvailable(network: Network) {
-                    //Log.e(TAG, "The default network is now: $network")
-                    Log.e(TAG, "onAvailable()")
+                    if (::onAvailableListener.isInitialized) {
+                        onAvailableListener.invoke()
+                    }
+                    Log.e(tag, "onAvailable() $network")
                 }
 
                 override fun onLost(network: Network) {
-
-                    Log.e(TAG, "onLost()")
+                    if (::onLostListener.isInitialized) {
+                        onLostListener.invoke()
+                    }
+                    Log.e(tag, "onLost()")
 //                    Log.e(
 //                        TAG,
 //                        "The application no longer has a default network. The last default network was $network"
@@ -66,8 +80,7 @@ object NetworkManager {
                         networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                     val validated =
                         networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-                    Log.e(TAG, "Internet: $internet - Validated: $validated")
-                    //Log.e(TAG, "The default network changed capabilities: $networkCapabilities")
+                    Log.e(tag, "Internet: $internet - Validated: $validated")
                 }
 
                 override fun onLinkPropertiesChanged(
